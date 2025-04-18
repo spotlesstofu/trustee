4a4b56efc2b65579ac972044c1a1a4bd95d2f1a7
Camilla Conte <cconte@redhat.com>
2025-04-18 11:25:22 +0200

    add trustee CLI
    a simple template to support:
    - new clap commands
    - aliases, where the same executable behaves differently according to
      the name it's called with
    - plugins, where some CLI commands map to other executables, allowing
      third-party extensions and easier distribution
    
    the goal of this CLI is to provide a single entry point to the trustee
    ecosystem and empower the user to explore and consume everything the
    trustee project has to offer
    
    Signed-off-by: Camilla Conte <cconte@redhat.com>

---
+++ tools/trustee/src/cli.rs
@@ -0,0 +1,32 @@
+use std::path::PathBuf;
+
+use anyhow::Result;
+use clap::{error::Error, Parser};
+use dirs::home_dir;
+
+#[derive(Debug, Parser)]
+enum Commands {}
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+    /// Path to the directory for storing config and data
+    #[arg(long, value_name = "PATH", env = "TRUSTEE_HOME")]
+    home: Option<PathBuf>,
+}
+
+pub async fn cli_default() -> Result<(), Error> {
+    let cli = Cli::try_parse()?;
+
+    let trustee_home_dir = cli
+        .home
+        .unwrap_or_else(|| home_dir().unwrap_or_default().join(".trustee"));
+
+    if !trustee_home_dir.exists() {
+        std::fs::create_dir_all(&trustee_home_dir)?;
+    }
+
+    Ok(())
+}
