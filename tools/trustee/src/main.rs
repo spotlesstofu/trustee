f080f9cddc5ac8c143178da047d3a9b5726bd244
Camilla Conte <cconte@redhat.com>
2025-04-18 11:25:22 +0200

    Add trustee CLI (tools/trustee)
    A simple template to support new clap commands.
    
    The goal of this CLI is to provide a single entry point to the trustee
    ecosystem and empower the user to explore and consume everything the
    trustee project has to offer.
    
    Signed-off-by: Camilla Conte <cconte@redhat.com>

---
+++ tools/trustee/src/main.rs
@@ -0,0 +1,13 @@
+use log::debug;
+
+mod cli;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    if let Err(e) = cli::cli_default().await {
+        eprintln!("Error: {:#}", e);
+        std::process::exit(1);
+    }
+}
