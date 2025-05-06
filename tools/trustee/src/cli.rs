d94f3f593f5d6a037b593f66ea08b57fa675b44e
Camilla Conte <cconte@redhat.com>
2025-05-06 17:29:03 +0200

    tools/trustee: default to deny_all policy
    In the `run` command, default to the deny_all policy if there is none
    configured. Intended to ease the deployment process as it allows to have
    a service up and running and complete the configuration gradually.
    
    Also add a new CLI argument `--allow-all` to use the allow_all policy
    instead. This one is intended for development.
    
    Signed-off-by: Camilla Conte <cconte@redhat.com>

--- tools/trustee/src/cli.rs
+++ tools/trustee/src/cli.rs
@@ -3,7 +3,7 @@ use std::path::{Path, PathBuf};
 use anyhow::Result;
 use clap::{error::Error, Parser};
 use dirs::home_dir;
-use log::info;
+use log::{info, warn};
 
 use crate::keys_certs::{new_auth_key_pair, new_https_certificate, new_https_key_pair};
 use kbs::{ApiServer, KbsConfig};
@@ -15,7 +15,11 @@ fn trustee_keygen(trustee_home_dir: &Path) -> Result<(PathBuf, PathBuf)> {
     Ok((private, public))
 }
 
-async fn trustee_run(config_file: Option<PathBuf>, trustee_home_dir: &Path) -> Result<()> {
+async fn trustee_run(
+    config_file: Option<PathBuf>,
+    allow_all: bool,
+    trustee_home_dir: &Path,
+) -> Result<()> {
     let mut config = config_file
         .map(|config_file| KbsConfig::try_from(config_file.as_path()).unwrap())
         .unwrap_or_default();
@@ -45,6 +49,21 @@ async fn trustee_run(config_file: Option<PathBuf>, trustee_home_dir: &Path) -> R
         }
     }
 
+    if allow_all {
+        warn!("Using policy allow_all. This is for development only.");
+        config.policy_engine.policy_path = trustee_home_dir.join("allow_all.rego");
+        std::fs::write(
+            &config.policy_engine.policy_path,
+            include_bytes!("../../../kbs/sample_policies/allow_all.rego"),
+        )?;
+    } else if !config.policy_engine.policy_path.exists() {
+        config.policy_engine.policy_path = trustee_home_dir.join("deny_all.rego");
+        std::fs::write(
+            &config.policy_engine.policy_path,
+            include_bytes!("../../../kbs/sample_policies/deny_all.rego"),
+        )?;
+    }
+
     let api_server = ApiServer::new(config).await?;
     // TODO initialize the components separately
     // and spawn each one within try_join
@@ -65,6 +84,10 @@ enum Commands {
         /// Configuration file
         #[arg(long)]
         config_file: Option<PathBuf>,
+        /// Use built-in policy to allow all (development only).
+        /// If neither this nor a policy file is provided, the default policy is to deny all.
+        #[arg(long)]
+        allow_all: bool,
     },
 }
 
@@ -94,8 +117,13 @@ pub async fn cli_default() -> Result<(), Error> {
             let out = out.unwrap_or_else(|| trustee_home_dir.join("key"));
             trustee_keygen(&out).unwrap();
         }
-        Commands::Run { config_file } => {
-            trustee_run(config_file, &trustee_home_dir).await.unwrap();
+        Commands::Run {
+            config_file,
+            allow_all,
+        } => {
+            trustee_run(config_file, allow_all, &trustee_home_dir)
+                .await
+                .unwrap();
         }
     };
 
