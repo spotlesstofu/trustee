e9881727c86ab11876e96422b3a19e79cc3c349d
Camilla Conte <cconte@redhat.com>
2025-05-06 17:17:24 +0200

    tools/trustee: add command run
    First commit to enable running Trustee with this CLI.
    
    The `run` command does the following:
    - Automatically create a key pair and use it for admin authentication
      if it doesn't exist in the configuration.
    - Import the kbs::ApiServer and start it in the foreground.
    
    Signed-off-by: Camilla Conte <cconte@redhat.com>

--- tools/trustee/src/cli.rs
+++ tools/trustee/src/cli.rs
@@ -6,12 +6,39 @@ use dirs::home_dir;
 use log::info;
 
 use crate::keys_certs::new_auth_key_pair;
+use kbs::{ApiServer, KbsConfig};
 
 fn trustee_keygen(trustee_home_dir: &Path) -> Result<(PathBuf, PathBuf)> {
     let (private, public) = new_auth_key_pair(trustee_home_dir)?;
     info!("Wrote new private key: {:?}", private);
     info!("Wrote new public key: {:?}", public);
     Ok((private, public))
+}
+
+async fn trustee_run(config_file: Option<PathBuf>, trustee_home_dir: &Path) -> Result<()> {
+    let mut config = if let Some(path) = config_file {
+        KbsConfig::try_from(path.as_path())?
+    } else {
+        KbsConfig::default()
+    };
+
+    if config.admin.auth_public_key.is_none() {
+        let key_base_path = trustee_home_dir.join("auth_key");
+        let private_path = key_base_path.with_extension("pem");
+
+        let public_path = {
+            if !private_path.exists() {
+                trustee_keygen(&key_base_path)?.1
+            } else {
+                key_base_path.with_extension("pub")
+            }
+        };
+        config.admin.auth_public_key = Some(public_path);
+    }
+
+    let api_server = ApiServer::new(config).await?;
+    api_server.server()?.await?;
+    Ok(())
 }
 
 #[derive(Debug, Parser)]
@@ -21,6 +48,12 @@ enum Commands {
         /// Output file for the private key
         #[arg(short = 'f')]
         output_file: Option<PathBuf>,
+    },
+    /// Launch Trustee. Uses a self-signed HTTPS certificate with a RSA 2048 key by default
+    Run {
+        /// Configuration file
+        #[arg(long)]
+        config_file: Option<PathBuf>,
     },
 }
 
@@ -53,6 +86,9 @@ pub async fn cli_default() -> Result<(), Error> {
         Commands::Keygen { output_file: out } => {
             let out = out.unwrap_or_else(|| trustee_home_dir.join("key"));
             trustee_keygen(&out).unwrap();
+        }
+        Commands::Run { config_file } => {
+            trustee_run(config_file, &trustee_home_dir).await.unwrap();
         }
     };
 
